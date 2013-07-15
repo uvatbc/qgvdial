06a265eb960007b982d541e8b49f38c2b3bffe3a
Yuvraaj Kelkar <yuvraaj@gmail.com>
2013-07-15 16:30:59 +0000

    first step to new arch

--- qgvdial/harmattan/main.cpp
+++ qgvdial/harmattan/main.cpp
@@ -2,7 +2,8 @@
 #include "qmlapplicationviewer.h"
 #include "MainWindow.h"
 
-Q_DECL_EXPORT int main(int argc, char *argv[])
+Q_DECL_EXPORT int
+main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
 
