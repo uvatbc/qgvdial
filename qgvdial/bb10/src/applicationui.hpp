df400741a2dff8788c43c780716336b9b1332583
Yuvraaj Kelkar <yuvraaj@gmail.com>
2013-07-25 00:00:55 +0000

    license for bb10 sources and first attempt at importing the api and common files

--- qgvdial/bb10/src/applicationui.hpp
+++ qgvdial/bb10/src/applicationui.hpp
@@ -1,8 +1,30 @@
+/*
+qgvdial is a cross platform Google Voice Dialer
+Copyright (C) 2009-2013  Yuvraaj Kelkar
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Contact: yuvraaj@gmail.com
+*/
+
 // Tabbed pane project template
-#ifndef ApplicationUI_HPP_
-#define ApplicationUI_HPP_
+#ifndef MainWindow_HPP_
+#define MainWindow_HPP_
 
 #include <QObject>
+#include "IMainWindow.h"
 
 namespace bb { namespace cascades { class Application; }}
 
@@ -11,12 +33,12 @@ namespace bb { namespace cascades { class Application; }}
  *
  *Use this object to create and init app UI, to create context objects, to register the new meta types etc.
  */
-class ApplicationUI : public QObject
+class MainWindow : public IMainWindow
 {
     Q_OBJECT
 public:
-    ApplicationUI(bb::cascades::Application *app);
-    virtual ~ApplicationUI() {}
+    MainWindow(QObject *app);
+    virtual ~MainWindow() {}
 };
 
-#endif /* ApplicationUI_HPP_ */
+#endif /* MainWindow_HPP_ */
