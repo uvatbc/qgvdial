03d8cd3aa6bf1273c5e3713fabacae195c5294ef
Yuvraaj Kelkar <yuvraaj@gmail.com>
2014-01-08 09:22:52 +0000

    path changed

--- qgvdial/bb10-qml/run-package.sh
+++ qgvdial/bb10-qml/run-package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make -j4
-blackberry-nativepackager -package qgvdial.bar bar-descriptor.xml -devMode -debugToken /media/drobo/uv/work/bb_dev_cert/z10-dbgtoken.bar
+blackberry-nativepackager -package qgvdial.bar bar-descriptor.xml -devMode -debugToken /media/drobo/uv/work/bb_dev_cert/2/z10-dbgtoken.bar
 
