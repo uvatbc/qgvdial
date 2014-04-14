e42e9a45bb1ae27b5c01d5c36b0b8cce08cb6889
Yuvraaj Kelkar <yuvraaj@gmail.com>
2014-04-14 06:17:01 +0000

    one more step

--- qgvdial/features/qml/qml5-sailfish/CQmlViewer.cpp
+++ qgvdial/features/qml/qml5-sailfish/CQmlViewer.cpp
@@ -1,3 +1,24 @@
+/*
+qgvdial is a cross platform Google Voice Dialer
+Copyright (C) 2009-2014  Yuvraaj Kelkar
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+Contact: yuvraaj@gmail.com
+*/
+
 #include "CQmlViewer.h"
 #include <sailfishapp.h>
 
@@ -29,18 +50,6 @@ CQmlViewer::onDeclStatusChanged(QQuickView::Status status)
     Q_WARN(QString("status = %1").arg (status));
     emit viewerStatusChanged (false);
 }//CQmlViewer::onDeclStatusChanged
-
-QQuickItem *
-CQmlViewer::rootObject()
-{
-    return m_view->rootObject();
-}//CQmlViewer::rootObject
-
-QQmlContext *
-CQmlViewer::rootContext() const
-{
-    return m_view->rootContext ();
-}//CQmlViewer::rootContext
 
 bool
 CQmlViewer::connectToChangeNotify(QObject *item, const QString &propName,
