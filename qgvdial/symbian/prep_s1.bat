9c8030cb2cc5aeae003e2f18965525f2ea4a8a66
uvatbc <uvatbc@users.noreply.github.com>
2025-01-20 14:48:57 -0800

    long live symbian

--- qgvdial/symbian/prep_s1.bat
+++
@@ -1,3 +0,0 @@
-del /f /q isS3Belle isS3
-echo stuff>../symbian/isS1
-echo stuff>../symbian/mqlib-build
--- qgvdial/symbian/prep_s3_belle.bat
+++
@@ -1,3 +0,0 @@
-del /f /q isS1 isS3
-echo stuff>../symbian/isS1
-echo stuff>../symbian/mqlib-build
