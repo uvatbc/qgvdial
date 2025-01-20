3c3fb4381d96de5a70400b04eac72b5f46cd2935
uvatbc <uvatbc@users.noreply.github.com>
2025-01-20 14:47:55 -0800

    long live bb10

--- qgvdial/bb10-qt5/run-package.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make -j4
-blackberry-nativepackager -package qgvdial.bar bar-descriptor.xml -devMode -debugToken ~/.rim/debugtoken1.bar
-
--- qgvdial/bb10-qt5/run-qmake.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-qmake bb10.pro
-#qmake -spec unsupported/blackberry-x86-g++ -o Makefile.x86 bb10.pro
-
